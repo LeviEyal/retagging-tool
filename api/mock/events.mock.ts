d6f72fdd81361f3148f67d4b15f227949ac5b9ac
Eyal Levi <happyeyal@gmail.com>
2022-10-22 10:55:32 +0300

    API: add mocked data

---
+++ api/mock/events.mock.ts
@@ -0,0 +1,130 @@
+import type { Event } from "../../common/types";
+
+export const events: Event[] = [
+	{
+		id: "1",
+		site: "Site 1",
+		lane: "Lane 1",
+		date: "2021-09-01T00:00:00.000Z",
+		topViewImageUrl: "https://picsum.photos/200/300",
+		sideViewImageUrl: "https://picsum.photos/200/300",
+		detections: [
+			{
+				imageUrl: "https://picsum.photos/200/300",
+				inspectFeedback: "No fruit",
+				reviewerFeedback: "No fruit",
+				seetrueFeedback: "No fruit"
+			},
+			{
+				imageUrl: "https://picsum.photos/200/300",
+				inspectFeedback: "No fruit",
+				reviewerFeedback: "No fruit",
+				seetrueFeedback: "No fruit"
+			}
+		]
+	},
+	{
+		id: "2",
+		site: "Site 2",
+		lane: "Lane 2",
+		date: "2021-09-02T00:00:00.000Z",
+		topViewImageUrl: "https://picsum.photos/200/300",
+		sideViewImageUrl: "https://picsum.photos/200/300",
+		detections: [
+			{
+				imageUrl: "https://picsum.photos/200/300",
+				inspectFeedback: "No fruit",
+				reviewerFeedback: "No fruit",
+				seetrueFeedback: "No fruit"
+			},
+			{
+				imageUrl: "https://picsum.photos/200/300",
+				inspectFeedback: "No fruit",
+				reviewerFeedback: "No fruit",
+				seetrueFeedback: "No fruit"
+			}
+		]
+	},
+	{
+		id: "3",
+		site: "Site 3",
+		lane: "Lane 3",
+		date: "2021-09-03T00:00:00.000Z",
+		topViewImageUrl: "https://picsum.photos/200/300",
+		sideViewImageUrl: "https://picsum.photos/200/300",
+		detections: [
+			{
+				imageUrl: "https://picsum.photos/200/300",
+				inspectFeedback: "No fruit",
+				reviewerFeedback: "No fruit",
+				seetrueFeedback: "No fruit"
+			},
+			{
+				imageUrl: "https://picsum.photos/200/300",
+				inspectFeedback: "No fruit",
+				reviewerFeedback: "No fruit",
+				seetrueFeedback: "No fruit"
+			}
+		]
+	},
+	{
+		id: "4",
+		site: "Site 4",
+		lane: "Lane 4",
+		date: "2021-09-04T00:00:00.000Z",
+		topViewImageUrl: "https://picsum.photos/200/300",
+		sideViewImageUrl: "https://picsum.photos/200/300",
+		detections: [
+			{
+				imageUrl: "https://picsum.photos/200/300",
+				inspectFeedback: "No fruit",
+				reviewerFeedback: "No fruit",
+				seetrueFeedback: "No fruit"
+			},
+			{
+				imageUrl: "https://picsum.photos/200/300",
+				inspectFeedback: "No fruit",
+				reviewerFeedback: "No fruit",
+				seetrueFeedback: "No fruit"
+			}
+		]
+	},
+	{
+		id: "5",
+		site: "Site 5",
+		lane: "Lane 5",
+		date: "2021-09-05T00:00:00.000Z",
+		topViewImageUrl: "https://picsum.photos/200/300",
+		sideViewImageUrl: "https://picsum.photos/200/300",
+		detections: [
+			{
+				imageUrl: "https://picsum.photos/200/300",
+				inspectFeedback: "No fruit",
+				reviewerFeedback: "No fruit",
+				seetrueFeedback: "No fruit"
+			},
+			{
+				imageUrl: "https://picsum.photos/200/300",
+				inspectFeedback: "No fruit",
+				reviewerFeedback: "No fruit",
+				seetrueFeedback: "No fruit"
+			}
+		]
+	},
+	{
+		id: "6",
+		site: "Site 6",
+		lane: "Lane 6",
+		date: "2021-09-06T00:00:00.000Z",
+		topViewImageUrl: "https://picsum.photos/200/300",
+		sideViewImageUrl: "https://picsum.photos/200/300",
+		detections: [
+			{
+				imageUrl: "https://picsum.photos/200/300",
+				inspectFeedback: "No fruit",
+				reviewerFeedback: "No fruit",
+				seetrueFeedback: "No fruit"
+			},
+		]
+	}
+];
