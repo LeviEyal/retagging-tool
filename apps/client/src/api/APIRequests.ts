41ee7b924021119d02752297d8ab3635308e8c5b
Eyal Levi <happyeyal@gmail.com>
2022-10-25 08:40:36 +0300

    Adding requests file

--- apps/client/src/api/APIRequests.ts
+++ apps/client/src/api/APIRequests.ts
@@ -1,11 +1,15 @@
-import type { Event } from "types";
+import type { Event } from "../../../../packages/types";
+
+const SERVER_URL = "http://localhost:7001/api";
 
 export const getEvents = async (): Promise<Event[]> => {
-	const response = await fetch("api/events");
+	const response = await fetch(`${SERVER_URL}/events`);
 	return response.json() as Promise<Event[]>;
 };
 
 export const getEvent = async (id: string): Promise<Event> => {
-	const response = await fetch(`api/events/${id}`);
+	const response = await fetch(`${SERVER_URL}/events/${id}`);
+	console.log(response);
+	
 	return response.json() as Promise<Event>;
 }
